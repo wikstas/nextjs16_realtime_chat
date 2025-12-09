e0b0f078241708dea72c683d6e6df161c29fdc08
RZH <166813579+wikstas@users.noreply.github.com>
2025-12-09 23:42:17 +0800

    Update client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden"
 import type { App } from "../app/api/[[...slugs]]/route"
 
-export const client = treaty<App>("https://nextjs16-realtime-chat.vercel.app").api
+export const client = treaty<App>("https://chat.r1cky.ggff.net").api
